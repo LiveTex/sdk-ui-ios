3f6d778f9d970aa03fc86099cd2990d5567a6b62
Iurii Kotikhin <kotikhinyv@gmail.com>
2023-04-20 08:17:54 +1000

    corrected package

--- Package.swift
+++ Package.swift
@@ -16,14 +16,15 @@ let package = Package(
         .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.0.0")),
         .package(url: "https://github.com/atone/BFRImageViewer",  from: "1.2.9"),
         .package(url: "https://github.com/pinterest/PINRemoteImage",  from: "3.0.3"),
-        .package(url: "https://github.com/LiveTex/sdk-ios", branch: "master")
+        //.package(url: "https://github.com/LiveTex/sdk-ios", branch: "master"),
+        .package(name: "LifetexCore", url: "https://github.com/LiveTex/sdk-ios", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LivetexUICore",
-            dependencies: ["MessageKit", "Kingfisher"],
+            dependencies: ["MessageKit", "Kingfisher", "LifetexCore"],
             path: "Sources"),
     ]
 )
