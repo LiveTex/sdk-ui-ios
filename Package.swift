2eac70174a69d7bfafb75b0522aeb99f38799121
Iurii Kotikhin <kotikhinyv@gmail.com>
2023-04-24 11:44:00 +1000

    no message

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://github.com/MessageKit/MessageKit", from: "4.1.1"),
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "6.3.1" ),
         .package(url: "https://github.com/atone/BFRImageViewer", from: "1.2.9"),
-        .package(url: "https://github.com/pinterest/PINRemoteImage", from: "3.0.3") //branch: "master"),
+        .package(url: "https://github.com/pinterest/PINRemoteImage", branch: "master"),
     ],
     targets: [
         .target(
